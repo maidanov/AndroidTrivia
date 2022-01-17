3e1aa8eab369e01a0506b55697bc61092c1df775
maidanov <80475448+maidanov@users.noreply.github.com>
2022-01-17 14:47:51 +0600

    Add files via upload

---
+++ src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidtrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.navigation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
